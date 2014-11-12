fc41030f3d9b07e944c4b63817fe3d4250969acf
Alwin Mark <alwin@atk-solutions.de>
2014-11-12 20:39:21 +0100

    temp

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,17 @@ ADD mysql.cnf /etc/mysql/conf.d/my.cnf
 ADD init.sql /tmp/init.sql
 RUN /usr/sbin/mysqld --bootstrap < /tmp/init.sql
 
+# VMail Directory
+RUN mkdir /var/vmail
+RUN useradd vmail
+RUN chown -R vmail:vmail /var/vmail
+RUN chmod -R 770 /var/vmail/
+
+# Dovecot installieren
+WORKDIR /etc/dovecot
+RUN apt-get install dovecot-core dovecot-imapd dovecot-mysql dovecot-lmtpd
+
+
 
 EXPOSE 3306
 CMD ["/usr/bin/supervisord"]
--- init.sql
+++ init.sql
@@ -2,3 +2,17 @@ USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("%password%") WHERE user='root';
+
+create database vmail;
+use vmail;
+
+# Tabelle für Benutzerkonten erstellen
+create table users (id INT UNSIGNED AUTO_INCREMENT NOT NULL, username VARCHAR(128) NOT NULL, domain VARCHAR(128) NOT NULL, password VARCHAR(128) NOT NULL, UNIQUE (id), PRIMARY KEY (username, domain) );
+
+# Tabelle für Domains
+create table domains (domain VARCHAR(128) NOT NULL, UNIQUE (domain));
+
+# Tabelle für Aliase
+create table aliases (id INT UNSIGNED AUTO_INCREMENT NOT NULL, source VARCHAR(128) NOT NULL, destination VARCHAR(128) NOT NULL, UNIQUE (id), PRIMARY KEY (source, destination) );
+
+GRANT ALL ON vmail.* TO 'vmail'@'localhost' IDENTIFIED BY 'vmailpasswort';
